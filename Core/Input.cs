4d7630e5aeea6720608210fddce9d5b6745b2d12
jmfp <therealjmfp@gmail.com>
2024-06-13 15:48:40 -0400

    Added four direction top down input

--- Core/Input.cs
+++ Core/Input.cs
@@ -21,6 +21,50 @@ namespace Sorcery.Core{
         public void TopDown8(){
             isPressed = false;
             movementVector = new Vector2(0, 0);
+            if(Keyboard.GetState().IsKeyDown(Keys.A)) {
+                isPressed = true;
+                movementVector.X -= maxSpeed;
+                //moving west
+                directions[3] = true;
+            }
+            if(Keyboard.GetState().IsKeyDown(Keys.D)) {
+                isPressed = true;
+                movementVector.X += maxSpeed;
+                //moving east
+                directions[1] = true;
+            }
+            if(Keyboard.GetState().IsKeyDown(Keys.W)) {
+                isPressed = true;
+                movementVector.Y -= maxSpeed;
+                //moving north
+                directions[0] = true;
+            }
+            if(Keyboard.GetState().IsKeyDown(Keys.S)) {
+                isPressed = true;
+                movementVector.Y += maxSpeed;
+                //moving south
+                directions[2] = true;
+            }
+            if(Keyboard.GetState().IsKeyUp(Keys.A) && isPressed) {
+                directions[3] = false;
+            }
+            if(Keyboard.GetState().IsKeyUp(Keys.D) && isPressed) {
+                directions[1] = false;
+            }
+            if(Keyboard.GetState().IsKeyUp(Keys.W) && isPressed) {
+                directions[0] = false;
+            }
+            if(Keyboard.GetState().IsKeyUp(Keys.S) && isPressed) {
+                directions[2] = false;
+            }
+
+            parent.position.X += movementVector.X;
+            parent.position.Y += movementVector.Y;
+        }
+
+        public void TopDown4(){
+            isPressed = false;
+            movementVector = new Vector2(0, 0);
             if(Keyboard.GetState().IsKeyDown(Keys.A) && !isPressed) {
                 isPressed = true;
                 movementVector.X -= maxSpeed;
@@ -62,6 +106,8 @@ namespace Sorcery.Core{
             parent.position.Y += movementVector.Y;
         }
     }
+
+    
 
     //makes game objects clickable, has nothing to do with UI
     public interface IClickable{
